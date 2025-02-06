911ecbb09d538f3cb7710281ac52238e087165b4
William Spackman <william.spackman@alumni.ucalgary.ca>
2025-02-06 13:39:23 -0700

    add version file

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
+import re
+
+with open('src/PriceIndexCalc/version.py') as version_file:
+    __version__, = re.findall('__version__ = "(.*)"', version_file.read())
 
 setup(
     name='PriceIndexCalc',
-    version='0.7.0',
+    version=__version__,
     description='Price Index Calculator using bilateral and multilateral methods',
     url='https://github.com/wspackman/PriceIndexCalc',
     license='MIT',
